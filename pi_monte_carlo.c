05f9b98d5d5b8e03390160a05aa52cdcb15230d9
andreamcm <cor16076@uvg.edu.gt>
2020-02-19 19:28:12 -0600

    Monte Carlo 1

--- pi_monte_carlo.c
+++ pi_monte_carlo.c
@@ -19,6 +19,13 @@
 #include <stdlib.h>
 #include <omp.h>
 #include "my_rand.h"
+#include <time.h>
+#include <math.h>
+
+#define MR_MULTIPLIER 279470273 
+#define MR_INCREMENT 0
+#define MR_MODULUS 4294967291U
+#define MR_DIVISOR ((double) 4294967291U)
 
 /* Serial function */
 void Get_args(char* argv[], int* thread_count_p, 
@@ -30,6 +37,7 @@ long long int Count_hits(long long int number_of_tosses, int thread_count);
 
 /*---------------------------------------------------------------------*/
 int main(int argc, char* argv[]) {
+   
    double pi_estimate;
    int thread_count;
    long long int number_in_circle;
@@ -46,6 +54,22 @@ int main(int argc, char* argv[]) {
    return 0;
 }
 
+unsigned my_rand(unsigned* seed_p) {
+   long long z = *seed_p;
+   z *= MR_MULTIPLIER; 
+// z += MR_INCREMENT;
+   z %= MR_MODULUS;
+   *seed_p = z;
+   return *seed_p;
+}
+
+double my_drand(unsigned* seed_p) {
+   unsigned x = my_rand(seed_p);
+   double y = x/MR_DIVISOR;
+   return y;
+}
+
+
 /*---------------------------------------------------------------------
  * Function:      Count_hits
  * Purpose:       Calculate number of hits in the unit circle
@@ -55,8 +79,20 @@ int main(int argc, char* argv[]) {
 
 long long int Count_hits(long long int number_of_tosses, int thread_count) {
 
-   ** TODO **
+  // ** TODO **
+  time_t t;
+  srand((unsigned) time(&t));
+  long long int aciertos_en_circulo = 0;
+  double x, y, distiancia_cuadrada;
+  #  pragma omp parallel for num_threads(thread_count) firstprivate(distiancia_cuadrada, x, y) shared(aciertos_en_circulo, number_of_tosses)
+  for (int toss = 0; toss < number_of_tosses; toss++){
+    x = my_drand(&thread_count);
+    y = my_drand(&thread_count);
+    distiancia_cuadrada = x*x + y*y;
+    if(distiancia_cuadrada <= 1) aciertos_en_circulo++;
+  }
   
+  return aciertos_en_circulo;
 }  /* Count_hits */
 
 /*---------------------------------------------------------------------
@@ -86,4 +122,5 @@ void Get_args(
    *thread_count_p = strtol(argv[1], NULL, 10);  
    *number_of_tosses_p = strtoll(argv[2], NULL, 10);
 }  /* Get_args */
+
 
