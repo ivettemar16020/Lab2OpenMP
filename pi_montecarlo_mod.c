a61f12307f49f1dcda1870b9bd5fb0368737fd13
Ivette Cardona <ivettecardona@ivettes-mbp.lan>
2020-02-22 20:45:36 -0600

    Comments

--- pi_montecarlo_mod.c
+++ pi_montecarlo_mod.c
@@ -1,3 +1,15 @@
+/* File:      pi_monte_carlo.c
+ * Purpose:   Estimate pi using OpenMP and a monte carlo method
+ * Authors:   Ivette Cardona,
+ * 			  Michelle Bloomfield & 
+ * 			  Andrea Cordon 
+ * Input:     None
+ * Output:    Estimate of pi
+ *
+ * Note:      The estimated value of pi depends on both the number of 
+ *            threads and the number of "tosses".  
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -9,13 +21,16 @@ void Get_args(char* argv[], int* thread_count_p,
       long long int* number_of_tosses_p);
 void Usage(char* prog_name);
 
+/* Random number generator */
 long double fRand(long double fMin, long double fMax)
 {
 	long double f = (double)rand() / RAND_MAX;
 	return fMin + f * (fMax - fMin);
 }
 
-
+/* Player is the function that calculates the number of hits in the unit circle.
+ * Equals to Count_hits on the original file.
+ */
 int player(int playersDarts)
 {
 	srand(time(NULL));
@@ -35,6 +50,7 @@ int player(int playersDarts)
 
 int main(int argc, char* argv[])
 {
+	// Vars to use
 	int thread_count;
 	long long int number_of_tosses;
 
@@ -42,6 +58,7 @@ int main(int argc, char* argv[])
 	long const double REAL_PI = 3.141592653589;
 	int score = 0, playersDarts;
 
+	// Parameters
 	if (argc != 3) Usage(argv[0]);
     Get_args(argv, &thread_count, &number_of_tosses);
 
@@ -49,6 +66,7 @@ int main(int argc, char* argv[])
 	playersDarts = number_of_tosses / thread_count;
 	double beginParallel = omp_get_wtime();
 
+	// OMP PARALLEL FOR 
 #pragma omp parallel for
 	for (int i = 1; i <= thread_count; i++)
 		score += player(playersDarts);
@@ -56,7 +74,10 @@ int main(int argc, char* argv[])
 	double endParallel = omp_get_wtime();
 	pi = 4.0 * ((long double)score / (long double)number_of_tosses);
 
+	// Time calculation 
 	double time_spent_parallel = endParallel - beginParallel;
+
+	// Output
 	printf("\n\t Calculated pi : %.5Lf\n", pi);
 	printf(	 "\t       Real pi : %.5Lf\n", REAL_PI);
 	printf("\n\t Execution Time: %f\n", time_spent_parallel);
